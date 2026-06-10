7c13ea1c6a0587b5458860a8326935cb95bf1d56
ysmish <ysmishkis@gmail.com>
2026-06-10 19:46:55 +0300

    ADDED COMMENTS

--- while_nos_rust/src/nos.rs
+++ while_nos_rust/src/nos.rs
@@ -1,3 +1,57 @@
+//planet smishky 1022 $ cargo run
+//   Compiling while_nos_rust v0.1.0 (/u/students/smishky/PL_ex3/while_nos_rust)
+//warning: variant `Iand` is never constructed
+//  --> src/ast.rs:12:5
+//   |
+// 6 | pub enum AExp {
+//   |          ---- variant in this enum
+//...
+//12 |     Iand(Box<AExp>, Box<AExp>),
+//   |     ^^^^
+//   |
+//   = note: `AExp` has derived impls for the traits `Clone` and `Debug`, but these are intentionally ignored during dead code analysis
+//   = note: `#[warn(dead_code)]` (part of `#[warn(unused)]`) on by default
+//
+//warning: variants `True`, `False`, `Beq`, and `And` are never constructed
+//  --> src/ast.rs:21:5
+//   |
+//20 | pub enum BExp {
+//   |          ---- variants in this enum
+//21 |     True,
+//   |     ^^^^
+//22 |     False,
+//   |     ^^^^^
+//23 |     Aeq(AExp, AExp),
+//24 |     Beq(Box<BExp>, Box<BExp>),
+//   |     ^^^
+//...
+//27 |     And(Box<BExp>, Box<BExp>),
+//   |     ^^^
+//   |
+//   = note: `BExp` has derived impls for the traits `Clone` and `Debug`, but these are intentionally ignored during dead code analysis
+//
+//warning: function `s2` is never used
+//  --> src/semantics.rs:78:8
+//   |
+//78 | pub fn s2() -> State {
+//   |        ^^
+//
+//warning: `while_nos_rust` (bin "while_nos_rust") generated 3 warnings
+//    Finished `dev` profile [unoptimized + debuginfo] target(s) in 1.58s
+//     Running `target/debug/while_nos_rust`
+//--- Running Test Cases in Rust ---
+//Test test1 - x = 1
+//Test test2 - x = 4
+//Test test3 - x = 7
+//Test test4 - x = 0
+//Test test4 - y = 120
+//Test test5 - a = 2688
+//Test test5 - b = 0
+//Test test5 - c = 0
+//Test test6 - x = 5
+//Test test7 - x = 80
+//Test test8 - x = 10
+
 use crate::ast::*;
 use crate::semantics::*;
 
